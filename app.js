a829cf1e0540ffac800be9afac5369b22b1599f5
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-08-27 15:10:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const express = require('express');
-const app     = express();
+const express       = require('express');
+const app           = express();
+
+const router        = require('./router/router');
+const common_router = require('./router/common_views');
 
 app.use('view engine', 'ejs');
 app.use()
 app.use()
 
-export.module = app;
+app.use('fiboapp/', router);
+app.use('/'       , common_router);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+exports.module = app;
